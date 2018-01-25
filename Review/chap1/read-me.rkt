b27449eefbb3b44f78fa5a87d8cbeaf8bfad0b4b
R. Yang Hwang <1285608014@qq.com>
2018-01-25 17:31:46 +0800

    Add files via upload

---
+++ Review/chap1/read-me.rkt
@@ -0,0 +1,50 @@
+; ------------------------------- Chapter 1 Toys -------------------------------
+; 
+; There are three parts of Toys.
+;
+; Part 1:
+;   (atom? x) 
+;
+; 
+;   (list? x)
+;
+; 
+; 
+;   (S-expression? x)
+; 
+; 
+;
+;   (how-many-S-expressions x)
+; 
+;
+;
+;   (display-sub-S-expressions x)
+; 
+;
+; Part 2:
+;   (car x)
+;
+; 
+; 
+;   (cdr x)
+; 
+; 
+;
+;   (cons s l)
+; 
+;
+; 
+; 
+;
+; Part 3:
+;   (null? x)
+;
+; 
+; 
+;   (eq? x y)
+; 
+; 
+;
+; 
+; 
+; ------------------------------------------------------------------------------
