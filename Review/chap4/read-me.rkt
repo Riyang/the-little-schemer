6ab368efb593d07876631fd6cfea5856d507ba83
R. Yang Hwang <1285608014@qq.com>
2018-02-16 00:33:31 +0800

    Create read-me.rkt

---
+++ Review/chap4/read-me.rkt
@@ -0,0 +1,36 @@
+; -------------------------- Chapter 4 Numbers Games ---------------------------
+;
+; There are four parts of chapter 4.
+;
+; Part 1:
+;         (add1 n)
+;         (sub1 n)
+;         (o+ n m)
+;         (o- n m)
+;
+; Part 2:
+;         (tup? l)
+;         (addtup tup)
+;         (o* n m)
+;         (tup+ tup1 tup2)
+;
+; Part 3:
+;         (o> n m)
+;         (o< n m)
+;         (o= n m)
+;         (^ n m)
+;         (o/ n m)
+;
+; Part 4:
+;         (length lat)
+;         (pick n lat)
+;         (rempick n lat)
+;         (no-nums lat)
+;         (all-nums lat)
+;         (eqan? a1 a2)
+;         (occur a lat)
+;         (one? n)
+;         (rempick n lat), which uses one?
+;
+;
+; ------------------------------------------------------------------------------
