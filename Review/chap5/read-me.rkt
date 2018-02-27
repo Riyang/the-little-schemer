b1a9782dab35b4e1185ffc2382f21379bd0a00bc
R. Yang Hwang <1285608014@qq.com>
2018-02-27 19:45:52 +0800

    Create read-me.rkt

---
+++ Review/chap5/read-me.rkt
@@ -0,0 +1,16 @@
+; ---------------- Chapter 5 *Oh My Gawd*: It's Full of Starts -----------------
+;
+; There are ? parts of chapter 5.
+;
+; Part 1:
+;         (rember* a l)
+;         (insertR* new old l)
+;         (occur* a l)
+;         (subst* new old l)
+;         (insertL* new old l)
+;         (member* a l)
+;
+; To be continue ...
+;
+;
+; ------------------------------------------------------------------------------
