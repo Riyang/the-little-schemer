e7715c9b8c8071699ab0d57fbbc16e95ded7ea7e
R. Yang Hwang <1285608014@qq.com>
2018-03-04 11:58:37 +0800

    Create read-me.rkt

---
+++ Review/chap6/read-me.rkt
@@ -0,0 +1,19 @@
+; ----------------------------- Chapter 6 Shadows ------------------------------
+;
+; There are 2 parts of chapter 6.
+;
+; Part 1:
+;         (numbered? aexp)
+;         (value nexp)
+;
+; Part 2:
+;         (sero? n)
+;         (edd1 n)
+;         (zub1 n)
+;         (o+ n m)
+;         (new-number? l)
+;         (etom? x)
+;         (new-lat? l)
+;
+;
+; ------------------------------------------------------------------------------
