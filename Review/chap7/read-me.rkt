edea2d65216040d32c91a144cf6fd46c2eefefd9
R. Yang Hwang <1285608014@qq.com>
2018-03-06 13:51:34 +0800

    Create read-me.rkt

---
+++ Review/chap7/read-me.rkt
@@ -0,0 +1,26 @@
+; ---------------------- Chapter 7 Friends and Relations -----------------------
+;
+; There are three parts of chapter 7.
+;
+; Part 1:
+;         (set? lat)
+;         (makeset lat)
+;         (subset? set1 set2)
+;         (eqset? set1 set2)
+;         (intersect? set1 set2)
+;         (intersect set1 set2)
+;         (union set1 set2);
+;
+; Part 2:
+;         (xxx set1 set2)
+;         (intersectall l-set)
+;
+; Part 3:
+;         (a-pair? x)
+;         (rel? l)
+;         (fun? rel)
+;         (fullfun? fun)
+;         (one-to-one? fun)
+;
+;
+; ------------------------------------------------------------------------------
