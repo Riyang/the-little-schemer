728a82ad49631cf721a3220e7f8817a3b4f6d12e
R. Yang Hwang <1285608014@qq.com>
2018-03-15 21:38:30 +0800

    Create read-me.rkt

---
+++ Review/chap9/read-me.rkt
@@ -0,0 +1,17 @@
+; -------------- Chapter 9 ...and Again, and Again, and Again,... --------------
+;
+; There are 3 parts of chapter 9.
+;
+; Part 1:
+;         (looking a lat)
+;         
+;
+; Part 2:
+;
+;
+; Part 3:
+;         
+;
+; To be continue...
+;
+; ------------------------------------------------------------------------------
